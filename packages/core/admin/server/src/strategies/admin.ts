e7707686d79f73f8c591ca7ff8debee45be09e7e
proactive-dev <proactive.d20@gmail.com>
2025-06-02 11:28:45 -0400

    fix to allow cross subdomain in strapi

--- packages/core/admin/server/src/strategies/admin.ts
+++ packages/core/admin/server/src/strategies/admin.ts
@@ -5,18 +5,23 @@ import { getService } from '../utils';
 export const authenticate = async (ctx: Context) => {
   const { authorization } = ctx.request.header;
 
-  if (!authorization) {
+  let token;
+  if (authorization) {
+    const parts = authorization.split(/\s+/);
+    if (parts[0].toLowerCase() !== 'bearer' || parts.length !== 2) {
+      if(!!ctx.cookies && !!ctx.cookies.get('jwtToken')) {
+        token = ctx.cookies.get('jwtToken')
+      } else {
+        return { authenticated: false };
+      }
+    } else {
+      token = parts[1];
+    }
+  } else {
     return { authenticated: false };
   }
 
-  const parts = authorization.split(/\s+/);
-
-  if (parts[0].toLowerCase() !== 'bearer' || parts.length !== 2) {
-    return { authenticated: false };
-  }
-
-  const token = parts[1];
-  const { payload, isValid } = getService('token').decodeJwtToken(token);
+  const { payload, isValid } = getService('token').decodeJwtToken(token || "");
 
   if (!isValid) {
     return { authenticated: false };
